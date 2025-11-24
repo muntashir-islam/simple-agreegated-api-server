7010ce0fdc64ba8be334899fce276aad1f46d99d
Muntashir Islam <muntashir.nirjhor@gmail.com>
2025-11-23 22:48:57 -0500

    Change http server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,7 +5,12 @@ import (
 )
 
 func main() {
-	server := New()
+	server := apiserver.New()
 	log.Println("Starting Aggregated API Server on :8443")
 	server.RunTLS(":8443")
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	log.Fatal(http.ListenAndServe(":8443", nil))
 }
